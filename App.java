7a6cb150370814cd828b188a287bf4c734582c0e
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-09 13:27:43 -0300

    testando o commit na branch "testando"

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Commit na master");
+
+        System.out.println("Testando o branch");
+        System.out.println("Fazendo uma ramificação");
     }
 }
