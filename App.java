ce5a6bd5656887cad5b7b2ad57158d2a193a9bf4
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-09 13:33:25 -0300

    commit na branch02

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Commit na master");
+
+        System.out.println("Usando a segunda branch");
+        System.out.println("Commit na branch02");
     }
 }
